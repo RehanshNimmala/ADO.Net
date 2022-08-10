c8a77ac7d95d5389f9eeab08a5640929923e46b1
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-10 14:36:22 -0500

    Save data from UI

---
+++ InsertUpdateDeletePractise/InsertUpdateDeletePractise/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace InsertUpdateDeletePractise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRollNumber = new System.Windows.Forms.Label();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.txtRollNumber = new System.Windows.Forms.TextBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRollNumber
+            // 
+            this.lblRollNumber.AutoSize = true;
+            this.lblRollNumber.Location = new System.Drawing.Point(93, 39);
+            this.lblRollNumber.Name = "lblRollNumber";
+            this.lblRollNumber.Size = new System.Drawing.Size(82, 16);
+            this.lblRollNumber.TabIndex = 0;
+            this.lblRollNumber.Text = "Roll Number";
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Location = new System.Drawing.Point(93, 103);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(92, 16);
+            this.lblStudentName.TabIndex = 1;
+            this.lblStudentName.Text = "Student Name";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(93, 169);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(32, 16);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "Age";
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Location = new System.Drawing.Point(93, 231);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(50, 16);
+            this.lblCourse.TabIndex = 3;
+            this.lblCourse.Text = "Course";
+            // 
+            // txtRollNumber
+            // 
+            this.txtRollNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRollNumber.Location = new System.Drawing.Point(252, 39);
+            this.txtRollNumber.Name = "txtRollNumber";
+            this.txtRollNumber.Size = new System.Drawing.Size(192, 22);
+            this.txtRollNumber.TabIndex = 4;
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtStudentName.Location = new System.Drawing.Point(252, 103);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(192, 22);
+            this.txtStudentName.TabIndex = 5;
+            // 
+            // txtAge
+            // 
+            this.txtAge.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAge.Location = new System.Drawing.Point(252, 163);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(192, 22);
+            this.txtAge.TabIndex = 6;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCourse.Location = new System.Drawing.Point(252, 231);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(192, 22);
+            this.txtCourse.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(28, 363);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(174, 363);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(322, 363);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(472, 363);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(607, 363);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.txtRollNumber);
+            this.Controls.Add(this.lblCourse);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblStudentName);
+            this.Controls.Add(this.lblRollNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRollNumber;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.TextBox txtRollNumber;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
