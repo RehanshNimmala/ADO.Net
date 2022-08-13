9b1c51d3a65ab0e9572e742ac7a8e8ac9b90e487
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-13 15:51:32 -0500

    Functionality added to grid view

---
+++ InsertUpdateDeletePractise/InsertUpdateDeletePractise/DataGridView.cs
@@ -0,0 +1,10 @@
+﻿namespace InsertUpdateDeletePractise
+{
+    internal class DataGridView
+    {
+        public int RollNumber { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public string Course { get; set; }  
+    }
+}
