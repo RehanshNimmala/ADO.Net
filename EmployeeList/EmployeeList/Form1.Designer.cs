e70733b4c26575318d757e341024d9289ffe37a6
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-19 20:34:21 -0500

    Employee_List added

---
+++ EmployeeList/EmployeeList/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace EmployeeList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmployeeId = new System.Windows.Forms.Label();
+            this.lblEmployeeName = new System.Windows.Forms.Label();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.txtEmployeeId = new System.Windows.Forms.TextBox();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.txtDepartment = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEmployeeId
+            // 
+            this.lblEmployeeId.AutoSize = true;
+            this.lblEmployeeId.Location = new System.Drawing.Point(48, 42);
+            this.lblEmployeeId.Name = "lblEmployeeId";
+            this.lblEmployeeId.Size = new System.Drawing.Size(85, 16);
+            this.lblEmployeeId.TabIndex = 0;
+            this.lblEmployeeId.Text = "Employee ID";
+            // 
+            // lblEmployeeName
+            // 
+            this.lblEmployeeName.AutoSize = true;
+            this.lblEmployeeName.Location = new System.Drawing.Point(48, 104);
+            this.lblEmployeeName.Name = "lblEmployeeName";
+            this.lblEmployeeName.Size = new System.Drawing.Size(109, 16);
+            this.lblEmployeeName.TabIndex = 1;
+            this.lblEmployeeName.Text = "Employee Name";
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Location = new System.Drawing.Point(48, 172);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(77, 16);
+            this.lblDepartment.TabIndex = 2;
+            this.lblDepartment.Text = "Department";
+            // 
+            // txtEmployeeId
+            // 
+            this.txtEmployeeId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmployeeId.Location = new System.Drawing.Point(234, 42);
+            this.txtEmployeeId.Name = "txtEmployeeId";
+            this.txtEmployeeId.Size = new System.Drawing.Size(100, 22);
+            this.txtEmployeeId.TabIndex = 3;
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmployeeName.Location = new System.Drawing.Point(234, 104);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(100, 22);
+            this.txtEmployeeName.TabIndex = 4;
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDepartment.Location = new System.Drawing.Point(234, 172);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.Size = new System.Drawing.Size(100, 22);
+            this.txtDepartment.TabIndex = 5;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(51, 253);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(211, 72);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtDepartment);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.txtEmployeeId);
+            this.Controls.Add(this.lblDepartment);
+            this.Controls.Add(this.lblEmployeeName);
+            this.Controls.Add(this.lblEmployeeId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmployeeId;
+        private System.Windows.Forms.Label lblEmployeeName;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.TextBox txtEmployeeId;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.TextBox txtDepartment;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ EmployeeList/EmployeeList/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace EmployeeList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Declaring the variables
+
+            try
+            {
+                int employeeId = int.Parse(txtEmployeeId.Text);
+                string employeeName = txtEmployeeName.Text;
+                string department = txtDepartment.Text;
+
+                //Establishing the connection string
+
+                string databaseConnection = $@"data source=Naveen;Initial catalog=ADONET;
+                                        persist security info=True;integrated security=SSPI";
+                SqlConnection sqlConnection = new SqlConnection(databaseConnection);
+                sqlConnection.Open();
+                SqlCommand cmd = sqlConnection.CreateCommand();
+                cmd.Connection = sqlConnection;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = $@"Insert into Employee_LIst(Emp_ID,Emp_Name,Department)
+                                values({employeeId},'{employeeName}','{department}')";
+                int rowsEffected = cmd.ExecuteNonQuery();
+                if (rowsEffected > 0)
+                {
+                    MessageBox.Show("Data Inserted");
+
+                }
+                else
+                {
+                    MessageBox.Show("Data not Inserted");
+                }
+                cmd.Connection.Close();
+            }
+            catch (Exception Ex)
+            {
+
+                MessageBox.Show("Please contact Administrator");
+            }
+            
+        }
+    }
+}
