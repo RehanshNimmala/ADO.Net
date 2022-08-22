04123d5152ec120c4995572d21dc0524c3ce3623
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-21 23:35:00 -0500

    Grid View binded

---
+++ EmployeeList/EmployeeList/EmployeeDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList
+{
+    internal class EmployeeDetails
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }  
+        
+        public string Department { get; set; }
+    }
+}
