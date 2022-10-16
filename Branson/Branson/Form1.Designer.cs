a594647c1a8a33891dd457fb56cf15e493dd6d12
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-10-15 20:44:42 -0500

    insert button

--- Branson/Branson/Form1.Designer.cs
+++ Branson/Branson/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.exitButton = new System.Windows.Forms.Button();
+            this.insertButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // exitButton
@@ -41,11 +42,21 @@
             this.exitButton.UseVisualStyleBackColor = true;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(136, 184);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(75, 23);
+            this.insertButton.TabIndex = 1;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.insertButton);
             this.Controls.Add(this.exitButton);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button insertButton;
     }
 }
 
