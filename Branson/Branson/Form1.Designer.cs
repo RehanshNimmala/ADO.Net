82f7ac511c9f3c0d2728b576c2415b8481557018
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-11-09 21:25:30 -0600

    exit added

--- Branson/Branson/Form1.Designer.cs
+++ Branson/Branson/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.nameErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
             this.deleteButton = new System.Windows.Forms.Button();
             this.updateButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.nameErrorProvider)).BeginInit();
             this.SuspendLayout();
             // 
@@ -144,11 +145,22 @@
             this.updateButton.UseVisualStyleBackColor = true;
             this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "&Exit1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.updateButton);
             this.Controls.Add(this.deleteButton);
             this.Controls.Add(this.rideComboBox);
@@ -180,6 +192,7 @@
         private System.Windows.Forms.ErrorProvider nameErrorProvider;
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
