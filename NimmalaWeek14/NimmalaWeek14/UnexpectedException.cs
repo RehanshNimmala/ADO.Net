2683306b566bcd378763653f3700eda8c572b92c
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-11-16 15:21:13 -0600

    Custom Exceptions project added

---
+++ NimmalaWeek14/NimmalaWeek14/UnexpectedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimmalaWeek14
+{
+    internal class UnexpectedException:Exception
+    {
+        public UnexpectedException(string message) : base(message)
+        {
+
+        }//overloaded ctor
+        public UnexpectedException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }// overloadedctor
+
+    }//class
+}//NAmespace
