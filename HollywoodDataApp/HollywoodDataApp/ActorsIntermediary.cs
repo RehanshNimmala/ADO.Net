a00d8457c6c2298ed6d4a3f0e5f1ae79450fc2a9
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-11-25 16:26:40 -0600

    intermediary class added

---
+++ HollywoodDataApp/HollywoodDataApp/ActorsIntermediary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HollywoodDataApp
+{
+     class ActorsIntermediary
+    {
+        //property for errors
+        public string LastError { get; set; }   
+
+        public DataTable GetAllActors()
+        {
+            HollywoodDataClass hwActors= new HollywoodDataClass();
+
+            try
+            {
+                string sqlString = "Select * from Actors";
+                DataSet actorsDataSet = hwActors.GetDataSet(sqlString);
+                return actorsDataSet.Tables[0];
+            }
+            catch (Exception ex)
+            {
+
+                LastError = ex.Message;
+                return null; ;
+            }
+
+        }//GetAllActors
+    }//class
+}//Namespace
