58a03646d74c450f4ffd54b903a3bff1b4d2db1c
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-12-03 22:38:47 -0600

    functionality added

--- DBConnectionPractice2/DBConnectionPractice2/RNRDataBase.cs
+++ DBConnectionPractice2/DBConnectionPractice2/RNRDataBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,44 @@ namespace DBConnectionPractice2
         public RNRDataBase()
         {
             InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RNRDataBase_Load(object sender, EventArgs e)
+        {
+            //Declare the connection string
+            //Dataset to hold the data
+            //create SQL statement
+            //Create connection string and send connection query as argument
+            //Open the connection
+            //Get sql adapter
+            //use adapter fill method to fill the data
+            //close the connection
+
+            try
+            {
+                string rnrConnection = ConfigurationManager.ConnectionStrings["DBConnectionPractice2.properties.settings.DBConnection"].ConnectionString;
+                DataSet rnrDataset = new DataSet();
+                string sqlString = $@"select ISBN, Title, Author from books";
+                SqlConnection sqlConnection = new SqlConnection(rnrConnection);
+                sqlConnection.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlString, rnrConnection);
+                sqlDataAdapter.Fill(rnrDataset);
+                sqlConnection.Close();
+                dataGridView.DataSource = rnrDataset.Tables[0];
+                dataGridView.AutoResizeColumns();
+                dataGridView.ReadOnly = true;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
