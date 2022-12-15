323c0f2354a2043f444098185370e4a8ce037d25
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-12-14 21:46:24 -0600

    RollDicePractice added

---
+++ RollDicePractice/RollDicePractice/DiceRoll.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollDicePractice
+{
+    public class DiceRoll
+    {
+        //Create a method which generates two random numbers for the incoming parameters and return the total
+        public int Roll(out int die1, out int die2)
+        {
+            Random random = new Random();
+            die1 = random.Next(1, 7);
+            die2 = random.Next(1, 7);
+            return die1+ die2;
+        }//Roll
+    }//class
+}//namespace
