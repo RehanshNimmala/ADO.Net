2cfe5e74ae83c77b0649330ff6b071fae46f362c
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-12-19 23:18:07 -0600

    interface class added

---
+++ VehicleFormPractice/VehicleFormPractice/IDrivable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleFormPractice
+{
+    //11111111111
+    interface  IDrivable
+    {
+        //This interface has one property and one method that returns a string and accepta two arguments
+
+        double MaximumSpeed { get; set; }
+
+        string Drive(double distance, double speedLimit);
+    }//interface
+}//namespace
