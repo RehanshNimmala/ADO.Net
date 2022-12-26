b101a47c71e3a4236e1b60efd4ea21b19e028b8c
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-12-25 23:06:42 -0600

    new project added

---
+++ PolymorphismPractice/PolymorphismPractice/ClassShape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice
+{
+    public class ClassShape:IPolyShapes
+    {
+        protected int xPosition;
+        protected int yPosition;
+        public ClassShape(int x, int y)
+        {
+            xPosition = x;
+            yPosition = y;
+        }//ctor
+        public string Draw()
+        {
+            return $"Drawing shape at X position:{xPosition},Y:{yPosition}.{Environment.NewLine} output from shape class.";
+        }//end Draw()
+    }//class
+}//namespace
