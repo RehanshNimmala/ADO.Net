61d26806198e004ab16ebd04f12f4f8b9617095f
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-12-26 22:39:16 -0600

    classes added

---
+++ PolymorphismPractice/PolymorphismPractice/ClassCircle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice
+{
+    public class ClassCircle:ClassShape
+    {
+        public ClassCircle(int x,int y):base(x,y)
+        {
+
+        }//ctor
+        public string FillColor(string color)
+        {
+            return $@"Fill Circle with color{color}{Environment.NewLine}Output from CircleClass";
+
+        }//FillColor
+    }//end class
+}//end namespace
---
+++ PolymorphismPractice/PolymorphismPractice/ClassRectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice
+{
+     class ClassRectangle: ClassShape
+    {
+        public ClassRectangle(int length, int width): base(length, width)
+        {
+
+        }
+        public ClassRectangle(int length):base(length, length)
+        {
+
+        }
+        public new string Draw()
+        {
+            if (base.xPosition == base.yPosition)
+                return $@"Drawing a square:Length{xPosition}.{Environment.NewLine}
+                    Square is a rectangle with equal sides.Output from Rectangle Class.
+                    {Environment.NewLine} Draw() method of rectangle class hides the base class method.
+             {Environment.NewLine}The Rectangle Class ctor has overloaded constructors:one for square and other for Rectangle";
+            else
+                return $@"Drawing rectangle:X{xPosition},Y:{yPosition}.
+                {Environment.NewLine}Output from Rectangle class.
+                {Environment.NewLine}Draw() method of rectangle class hides the base class method";
+        }
+    }
+}
