e8382c715568ddf864a0a48a43f4a94d41f08dc3
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-12-28 00:19:00 -0600

    functions to controls added

--- PolymorphismPractice/PolymorphismPractice/Polymorphism.cs
+++ PolymorphismPractice/PolymorphismPractice/Polymorphism.cs
@@ -16,5 +16,35 @@ namespace PolymorphismPractice
         {
             InitializeComponent();
         }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void shapeButton_Click(object sender, EventArgs e)
+        {
+            ClassShape shape = new ClassShape(20, 30);
+            resultTextBox.Text = shape.Draw();
+
+        }
+
+        private void circleButton_Click(object sender, EventArgs e)
+        {
+            ClassCircle circle= new ClassCircle(20, 30);
+            resultTextBox.Text= circle.Draw();
+            resultTextBox.Text += $"Circle Class Draw method is inherited from ShapeClass.{Environment.NewLine}";
+            resultTextBox.Text += circle.FillColor("Red");
+
+        }//circle
+
+        private void rectangleButton_Click(object sender, EventArgs e)
+        {
+            ClassRectangle rectangle= new ClassRectangle(20, 30);
+            ClassRectangle square = new ClassRectangle(20);
+            resultTextBox.Text= rectangle.Draw();
+            resultTextBox.Text += Environment.NewLine;
+            resultTextBox.Text += square.Draw();
+        }
     }
 }
