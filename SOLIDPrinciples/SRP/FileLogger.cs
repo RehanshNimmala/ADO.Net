8f46a4196817afbfecb7a04c15fc57a793d0c38e
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2023-01-20 00:00:49 -0600

    console

---
+++ SOLIDPrinciples/SRP/FileLogger.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace SRP
+{
+    public static class FileLogger
+    {
+        public static void ErrorLOgger(string error)
+        {
+            File.WriteAllText(@"c:\Error.txt", error);
+        }
+        public static void ErrorLOgger(string error, string message)
+        {
+            File.WriteAllText(@"c:\Error.txt", error + message);
+        }
+        public static void DatabaseLogger(string message)
+        {
+            System.Console.WriteLine("database logger");
+            System.Console.WriteLine("test1");
+        }
+    }
+}
---
+++ SOLIDPrinciples/SRP/Membership.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SRP
+{
+    public class Membership
+    {
+        int a = 10;
+        int b = 10;
+        public void Add()
+        {
+            try
+
+            {
+                int c = a + b;
+            }
+
+            catch (Exception ex)
+            {
+                // FileLogger fileLogger = new FileLogger();
+                //fileLogger.ErrorLOgger(ex.Message);
+                //File.WriteAllText(@"c:\Error.txt", ex.Message);
+                FileLogger.ErrorLOgger(ex.Message);
+            }
+        }
+    }
+}
